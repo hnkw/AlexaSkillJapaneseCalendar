bbb7c2ba0da1dfcd2f25b3f78ca5310a0399ca3a
Kenji Hanakawa <hnkw@mac.com>
2018-02-11 10:10:24 +0900

    Use official zip tool.

--- lambda.sh
+++ lambda.sh
@@ -11,32 +11,38 @@ _build_pkg() {
 
 	(
 		cd "${base_dir}/lambda/$pkg"
-		GOOS="${os}" GOARCH="${arch}" go build -o "${base_dir}/build/${os}/${arch}/${pkg}"
+		GOOS="${os}" GOARCH="${arch}" go build
 	)
 }
 
 _build() {
 	base_dir="$1"
 
-	rm -rf "${base_dir}/build"
 	_build_pkg "${base_dir}" "convert"
+}
+
+_deploy_pkg() {
+	base_dir="$1"
+	pkg="$2"
+
+	_build_pkg "${base_dir}" "${pkg}"
+
+	(
+		cd "${base_dir}/lambda/$pkg"
+		build-lambda-zip "${pkg}"
+		aws lambda update-function-code --function-name JapaneseCalendarSkill --zip-file "fileb://./${pkg}.zip"
+	)
 }
 
 _deploy() {
 	base_dir="$1"
 
-	_build "${base_dir}"
-
-	deploy_zip_path="${base_dir}/deploy.zip"
-	(
-		cd "${base_dir}/build/${os}/${arch}"
-		zip "${deploy_zip_path}" ./*
-	)
-	aws lambda update-function-code --function-name JapaneseCalendarSkill --zip-file "fileb://${deploy_zip_path}"
-	rm "${deploy_zip_path}"
+	_deploy_pkg "${base_dir}" "convert"
 }
 
 _main() {
+	go get github.com/aws/aws-lambda-go/cmd/build-lambda-zip
+
 	base_dir=$(cd "$(dirname $0)" || exit 1 ; pwd)
 
 	sub_cmd="$1"
