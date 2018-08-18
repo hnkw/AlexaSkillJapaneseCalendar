4debdf1bb88848ef14c734e453c06b9e5fe687c1
Kenji Hanakawa <hnkw@mac.com>
2018-08-18 11:45:15 +0900

    Fix lintitg

--- lambda.sh
+++ lambda.sh
@@ -44,8 +44,8 @@ _deploy() {
 _main() {
 	go get github.com/aws/aws-lambda-go/cmd/build-lambda-zip
 
-	base_dir=$(cd "$(dirname $0)" || exit 1 ; pwd)
-
+	base_dir=$(cd "$(dirname "$0")" || exit 1 ; pwd)
+	
 	sub_cmd="$1"
 	shift
 	case $sub_cmd in
